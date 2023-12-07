4320df543af328e3825dd4f4b10c0da9020e8c46
Arulprakash <nirmalaarulprakashkaavika@Kaavika.local>
2023-12-07 14:32:21 +0530

    UI & menu changes

---
+++ src/app/config.const.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+export const imageAssetPrefix = 'assets/images/';
+
+export const defaultGeneralMenu = [
+  {
+    itemName: 'Dashboard',
+    itemURL: '/dashboard',
+    iconUrl: imageAssetPrefix + 'user.svg',
+    itemId: 'gm1',
+  },
+  {
+    itemName: 'Platforms',
+    itemURL: '/platform-details',
+    iconUrl: imageAssetPrefix + 'campaign.svg',
+    itemId: 'gm2',
+  },
+];
--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.css'],
 })
-export class MenuBarComponent {}
+export class MenuBarComponent {
+  generalMenu: any;
+  filteredGeneralMenu: any;
+  groupPackage;
+
+  constructor(public dataService: DataService) {
+    this.groupPackage = this.dataService.getGroupPackageInfo();
+  }
+
+  ngOnInit(): void {
+    this.generalMenu = this.groupPackage.generalMenu;
+    this.filteredGeneralMenu = this.groupPackage.generalMenu;
+  }
+}
